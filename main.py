9dd45cac517078403e4fa13b97d2d814895c5f8f
Vinicius Xavier <viniciusxr89@gmail.com>
2025-10-29 09:41:23 -0300

    add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Algoritmo A* para encontrar o menor caminho em um labirinto 2D entre dois pontos, evitando obstáculos e considerando os custos dos movimentos. 
+
+"""
+
+if __name__ == "__main__":
+    print("Algoritmo A*")
